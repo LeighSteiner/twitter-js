eead91bc4df69249826adb71ee71366b1efcfdef
Leigh Steiner <steinerleigh@gmail.com>
2017-06-13 10:00:08 -0400

    kelaiya joins us!

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ app.listen(3000, function(){
 	console.log('listening on port 3000…');
 });
 
+app.use(function (req, res, next) {
+	// var volleyball = require('volleyball');
+	// var theBall = volleyball();
+    // do your logging here
+    console.log(req.method, req.url,req.statusCode);
+    // console.log(Object.keys(req));
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    next();
+})
+app.use('/special/', function(req,res,next){
+	console.log('you reached the special area');
+	next();
+})
+app.get('/special/', function(req,res){
+	res.send('arent you special');
+})
 app.get('/', function (req, res) {
 	console.log('what a get!');
   res.send('Welcome to my TwitterClone!')
-})
+});
+
+app.get('/news', function (req, res) {
+	console.log('news get');
+  res.send('Good morning, heres the news! And all the news is good!');
+});
+
