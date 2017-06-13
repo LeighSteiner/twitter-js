5aee8865144c1c3c3909ff1a5f3e19973ce09359
Leigh Steiner <steinerleigh@gmail.com>
2017-06-13 10:53:24 -0400

    first render working

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require( 'express' );
 const app = express();
+const nunjucks = require('nunjucks')
 app.listen(3000, function(){
 	console.log('listening on port 3000…');
 });
 // Kelaiya is here yay!!
+var locals = {
+    title: 'Classmates',
+    people: [
+        { name: 'Emily'},
+        { name: 'Danni' },
+        { name: 'Omri'}
+    ]
+};
+const people = [{name: 'Your Majesty'}, {name: 'Hannah Baker'}, {name: 'Clay'}];
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});  // point nunjucks to the proper directory for templates
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
 
 app.use(function (req, res, next) {
 	// var volleyball = require('volleyball');
@@ -23,7 +39,7 @@ app.get('/special/', function(req,res){
 })
 app.get('/', function (req, res) {
 	console.log('what a get!');
-  res.send('Welcome to my TwitterClone!')
+  res.render( 'index', {title: 'Kelaiyas Favorite TV Characters', people: people});
 });
 
 app.get('/news', function (req, res) {
