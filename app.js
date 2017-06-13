77581825e7b920a67eaa13f69abbde227d6e3bd5
Leigh Steiner <steinerleigh@gmail.com>
2017-06-13 12:15:11 -0400

    first live version

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require( 'express' );
 const app = express();
 const nunjucks = require('nunjucks')
+const routes = require('./routes');
+
 app.listen(3000, function(){
 	console.log('listening on port 3000…');
 });
 // Kelaiya is here yay!!
-var locals = {
-    title: 'Classmates',
-    people: [
-        { name: 'Emily'},
-        { name: 'Danni' },
-        { name: 'Omri'}
-    ]
-};
-const people = [{name: 'Your Majesty'}, {name: 'Hannah Baker'}, {name: 'Clay'}];
+// var locals = {
+//     title: 'Classmates',
+//     people: [
+//         { name: 'Emily'},
+//         { name: 'Danni' },
+//         { name: 'Omri'}
+//     ]
+// };
+// const people = [{name: 'Your Majesty'}, {name: 'Hannah Baker'}, {name: 'Clay'}];
+// const leighPeople = [{name: "Melanie"}, {name: 'Elizabeth'}, {name: 'Mister Wednesday'}];
+app.use('/', routes);
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true});  // point nunjucks to the proper directory for templates
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});
+// nunjucks.render('index.html',function (err, output) {
+//     console.log(output);
+// });
 
 app.use(function (req, res, next) {
 	// var volleyball = require('volleyball');
@@ -34,16 +38,24 @@ app.use('/special/', function(req,res,next){
 	console.log('you reached the special area');
 	next();
 })
-app.get('/special/', function(req,res){
-	res.send('arent you special');
-})
-app.get('/', function (req, res) {
-	console.log('what a get!');
-  res.render( 'index', {title: 'Kelaiyas Favorite TV Characters', people: people});
-});
+app.use(express.static('public')); 
 
-app.get('/news', function (req, res) {
-	console.log('news get');
-  res.send('Good morning, heres the news! And all the news is good!');
-});
+app.get('/stylesheets/style.css', function(req,res){
+	res.sendFile('/Users/Klen/twitter-js/public/stylesheets/style.css');
+})
+// app.get('/special/', function(req,res){
+// 	res.send('arent you special');
+// })
+// app.get('/', function (req, res) {
+// 	console.log('what a get!');
+//   res.render( 'index', {title: 'Leighs Favorite TV Characters', people: leighPeople});
+// });
+
+// app.get('/news', function (req, res) {
+// 	console.log('news get');
+//   res.send('Good morning, heres the news! And all the news is good!');
+// });
+// app.get('/tweet/5', function(req,res){
+// 	res.send('<h1>Our Very First Tweet!</h1>');
+// })
 
